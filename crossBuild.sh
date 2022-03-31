5c36b1a02b7c65285d680ce77e2addf948781303
David Martinez <dmg103@alu.ua.es>
2022-03-31 16:51:17 +0200

    [master]: Cross compiling for switch

--- crossBuild.sh
+++ crossBuild.sh
@@ -24,6 +24,16 @@ cd ..
 mv ./build/libreactphysics3d.a ./build/libreactphysics3dWin.a
 cp -r ./build/libreactphysics3dWin.a ./bin/
 
+#Build switch
+rm -r build/
+mkdir build
+cd build
+cmake -D CMAKE_CXX_COMPILER=/opt/devkitpro/devkitA64/bin/aarch64-none-elf-g++ ..
+make -j4
+cd ..
+mv ./build/libreactphysics3d.a ./build/libreactphysics3dSwitch.a
+cp -r ./build/libreactphysics3dSwitch.a ./bin/
+
 
 
 
